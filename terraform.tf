b9835da8904726fddaeb754e07b8d0733ccf034d
anjathiart <anjathiart@gmail.com>
2024-07-23 17:04:20 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  /* cloud {
+    organization = "Frankenstein_Molly_Evie"
 
     workspaces {
       name = "learn-terraform"
     }
-  }
+  } */
 
   required_providers {
     aws = {
